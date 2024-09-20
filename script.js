9cabc4b4e05bcba3de73934eba5d9ab6947d7f9c
Your name <Your email>
2024-09-20 15:38:36 +0200

    test

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 fetch('https://deploy-me-to-vercel-be-h2ow.vercel.app/year')
  .then(response => response.json())
  .then(data => {
-   document.querySelector('#year').textContent = data.year;
+   document.querySelector('#year').textContent = `${data.year}`;
  });
